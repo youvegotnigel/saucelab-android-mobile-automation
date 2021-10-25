e9b6f88b8c2bf160be5b65e06f559a6b02f00076
Nigel Mulholland <nigeldave19@gmail.com>
2021-10-25 20:18:23 +0530

    Update LoginPage.java

--- src/main/java/com/saucelabs/mobile/ui/pageobjects/LoginPage.java
+++ src/main/java/com/saucelabs/mobile/ui/pageobjects/LoginPage.java
@@ -1,5 +1,7 @@
 package com.saucelabs.mobile.ui.pageobjects;
 
+import org.openqa.selenium.By;
+
 public class LoginPage extends BasePage{
 
     //Locators
@@ -10,6 +12,8 @@ public class LoginPage extends BasePage{
     private final String ACID_STANDARD_USER = "test-standard_user";
     private final String ACID_LOCKED_OUT_USER = "test-locked_out_user";
     private final String ACID_PROBLEM_USER = "test-problem_user";
+
+    private final By XPATH_LOCKED_OUT_USER_ERROR_MSG = By.xpath("//android.view.ViewGroup[@content-desc=\"test-Error message\"]/android.widget.TextView");
 
 
     public String get_ACID_USERNAME(){
@@ -34,5 +38,9 @@ public class LoginPage extends BasePage{
 
     public String get_ACID_PROBLEM_USER(){
         return ACID_PROBLEM_USER;
+    }
+
+    public By get_XPATH_LOCKED_OUT_USER_ERROR_MSG(){
+        return XPATH_LOCKED_OUT_USER_ERROR_MSG;
     }
 }
